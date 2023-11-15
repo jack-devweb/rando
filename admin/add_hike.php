14c069f9074815ab8ce55d8c8645f39fce21b945
jack-devweb <skywatchh@gmail.com>
2023-11-15 15:48:46 +0100

    J'ai modifié le formulaire et la récupération des colonnes

--- admin/add_hike.php
+++ admin/add_hike.php
@@ -3,23 +3,39 @@ require_once(__DIR__ . '/../includes/db/db.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupérer les données du formulaire
-    $hike_name = $_POST['hike_name'];
-    $hike_description = $_POST['hike_description'];
+    $hike_name = $_POST['hike_name'] ?? '';
+    $hike_description = $_POST['hike_description'] ?? '';
 
-
-    // Requête pour insérer une nouvelle randonnée dans la base de données
-    $query = "INSERT INTO randonnees (nom, description) VALUES (:nom, :description)";
-    $stmt = $connexion->prepare($query);
-    $stmt->bindParam(':nom', $hike_name);
-    $stmt->bindParam(':description', $hike_description);
-
-
-
-    if ($stmt->execute()) {
-        header('Location: admin.php');
+    // Vérification des données obligatoires
+    if (empty($hike_name) || empty($hike_description)) {
+        echo "Veuillez remplir tous les champs obligatoires.";
         exit();
-    } else {
-        echo "Erreur lors de l'ajout de la randonnée.";
+    }
+
+    try {
+        // Début de la transaction
+        $connexion->beginTransaction();
+
+        // Requête pour insérer une nouvelle randonnée dans la base de données
+        $query = "INSERT INTO randonnees (nom, description) VALUES (:nom, :description)";
+        $stmt = $connexion->prepare($query);
+        $stmt->bindParam(':nom', $hike_name);
+        $stmt->bindParam(':description', $hike_description);
+
+        if ($stmt->execute()) {
+            // Validation et envoi des modifications à la base de données
+            $connexion->commit();
+            header('Location: admin.php');
+            exit();
+        } else {
+            // En cas d'échec, annuler les modifications
+            $connexion->rollBack();
+            echo "Erreur lors de l'ajout de la randonnée.";
+        }
+    } catch (PDOException $e) {
+        // Gestion des exceptions
+        $connexion->rollBack();
+        echo "Erreur lors de l'exécution de la requête : " . $e->getMessage();
     }
 }
 ?>
--- admin/admin.php
+++ admin/admin.php
@@ -13,13 +13,25 @@
 
 <!-- Formulaire pour ajouter de nouvelles randonnées -->
 <h2>Ajouter une nouvelle randonnée</h2>
-<form method="post" action="">
+<form method="post" action="" enctype="multipart/form-data">
     <label for="hike_name">Nom :</label><br>
     <input type="text" id="hike_name" name="hike_name" required><br>
+    
     <label for="hike_description">Description :</label><br>
     <textarea id="hike_description" name="hike_description" required></textarea><br>
+    
+    <label for="departure_address">Adresse de départ :</label><br>
+    <input type="text" id="departure_address" name="departure_address" required><br>
+    
+    <label for="popularity_score">Score de popularité :</label><br>
+    <input type="number" id="popularity_score" name="popularity_score" min="1" max="5" required><br>
+    
+    <label for="hike_photo">Photo :</label><br>
+    <input type="file" id="hike_photo" name="hike_photo"><br>
+    
     <input type="submit" value="Ajouter" name="add_hike">
 </form>
+
 
 <?php
 try {
@@ -29,25 +41,43 @@ try {
     echo "Erreur de connexion : " . $e->getMessage();
 }
 
-// Vérifier si le formulaire d'ajout de randonnée est soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_hike'])) {
-    // Récupérer les données du formulaire
-    $hike_name = $_POST['hike_name'];
-    $hike_description = $_POST['hike_description'];
+    $hike_name = $_POST['hike_name'] ?? '';
+    $hike_description = $_POST['hike_description'] ?? '';
+    $departure_address = $_POST['departure_address'] ?? '';
+    $popularity_score = $_POST['popularity_score'] ?? '';
+    $hike_photo = $_FILES['hike_photo'] ?? null;
 
-    // Requête pour insérer une nouvelle randonnée dans la base de données
-    $query = "INSERT INTO randonnees (nom, description) VALUES (:nom, :description)";
-    $stmt = $connexion->prepare($query);
-    $stmt->bindParam(':nom', $hike_name);
-    $stmt->bindParam(':description', $hike_description);
+    if ($hike_photo && $hike_photo['name'] !== '') {
+        $photo_name = $hike_photo['name'];
+        $target_dir = __DIR__ . "/../uploads/"; // Chemin absolu vers le dossier des images
+        $target_file = $target_dir . basename($photo_name);
 
-    // Exécution de la requête d'insertion
-    if ($stmt->execute()) {
-        echo "<p>Randonnée ajoutée avec succès!</p>";
+        if (move_uploaded_file($hike_photo['tmp_name'], $target_file)) {
+            $query = "INSERT INTO randonnees (nom, description, adresse_depart, popularite, photo) VALUES (:nom, :description, :adresse_depart, :popularite, :photo)";
+            $stmt = $connexion->prepare($query);
+            $stmt->bindParam(':nom', $hike_name);
+            $stmt->bindParam(':description', $hike_description);
+            $stmt->bindParam(':adresse_depart', $departure_address);
+            $stmt->bindParam(':popularite', $popularity_score);
+            $stmt->bindParam(':photo', $photo_name);
+
+            if ($stmt->execute()) {
+                echo "<p>Randonnée ajoutée avec succès!</p>";
+            } else {
+                echo "<p>Erreur lors de l'ajout de la randonnée.</p>";
+            }
+        } else {
+            echo "<p>Erreur lors de l'upload de la photo.</p>";
+        }
     } else {
-        echo "<p>Erreur lors de l'ajout de la randonnée.</p>";
+        echo "<p>Veuillez sélectionner une photo.</p>";
     }
 }
+
+// Reste du code pour la suppression et l'affichage des randonnées...
+
+
 
 // Vérifier si une demande de suppression de randonnée est reçue
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
@@ -65,10 +95,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
     }
 }
 
-// Affichage de la liste des randonnées
+// Afficher les randonnées dans le tableau
 echo "<h2>Liste des randonnées</h2>";
 echo "<table>";
-echo "<thead><tr><th>Nom</th><th>Description</th><th>Action</th></tr></thead>";
+echo "<thead><tr><th>Nom</th><th>Description</th><th>Adresse de départ</th><th>Score de popularité</th><th>Photo</th><th>Action</th></tr></thead>";
 echo "<tbody>";
 
 // Requête pour sélectionner toutes les randonnées
@@ -81,11 +111,15 @@ foreach ($randonnees as $randonnee) {
     echo "<tr>";
     echo "<td>" . $randonnee['nom'] . "</td>";
     echo "<td>" . $randonnee['description'] . "</td>";
+    echo "<td>" . $randonnee['adresse_depart'] . "</td>";
+    echo "<td>" . $randonnee['popularite'] . "</td>";
+    echo "<td><img src='/projet-randonnee/uploads/" . $randonnee['photo'] . "' alt='Photo de la randonnée' style='width: 100px; height: 100px;'></td>";
     echo "<td><a href='delete_hike.php?id=" . $randonnee['id'] . "'>Supprimer</a></td>";
     echo "</tr>";
 }
 
 echo "</tbody></table>";
+
 
 ?>
 
